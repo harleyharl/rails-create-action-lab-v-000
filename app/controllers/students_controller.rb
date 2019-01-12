f799ea242b66dbab6f0248610aa9453b0713a868
Harley Rayner <harleymteden@gmail.com>
2019-01-12 18:22:26 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
 
   def new
     # binding.pry
-    @student = Student.new
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to new_student_path(@student)
   end
 
